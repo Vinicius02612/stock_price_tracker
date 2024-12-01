dd7c30fab37123300d8b16705a39b5dcdb310ffd
Vinicius Nunes <viniciusnunes02612@gmail.com>
2024-12-01 20:09:08 -0300

    configuration for chartsjs in project

---
+++ stock_price/src/components/ChartComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <line-chart :chart-data="chartData" :options="chartOptions"></line-chart>
+  </div>
+</template>
+
+<script>
+import { Line } from 'vue-chartjs'
+import { defineComponent } from 'vue'
+import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale,PointElement } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement)
+
+
+export default defineComponent({
+  name: 'ChartComponent',
+  components: {
+    LineChart: Line
+  },
+  props: {
+    chartData: {
+      type: Object,
+      required: true
+    },
+    chartOptions: {
+      type: Object,
+      required: true,
+      default: () => ({})
+    }
+  }
+})
+</script>
+
+<style scoped>
+/* Adicione seu estilo aqui */
+</style>
